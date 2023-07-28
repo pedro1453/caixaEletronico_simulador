5b41c7b54092b6f1ab626abfe96f8a7455090c54
pedro1453 <pedrojc1904@gmail.com>
2023-07-28 11:01:17 -0400

    criei uma função para printar os dados

--- codigo_python.py
+++ codigo_python.py
@@ -4,23 +4,14 @@
 #Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50, quatro notas de 10,
 #uma nota de 5 e quatro notas de 1.
 
-dinheiro = int(input("Informe o valor a ser sacado: R$"))
-
-if dinheiro > 10 and dinheiro <= 600:
-    notas_100 = int(dinheiro/100)
-    notas_50 = int((dinheiro % 100)/50)
-    notas_10 = int(((dinheiro % 100) % 50)/ 10)
-    notas_5 = int((((dinheiro % 100) % 50) % 10)/5)
-    moeda_1 = int(dinheiro - (notas_100*100 + notas_50*50 + notas_10*10 + notas_5*5))
-
-print("Para sacar a quantia de R$ {}, será necessário:".format(dinheiro))
-if notas_100 > 0:
-    print("Notas de 100: {}".format(notas_100))
-if notas_50 > 0:
-    print("Notas de 50: {}".format(notas_50))
-if notas_10 > 0: 
-    print("Notas de 10:{}".format(notas_10))
-if notas_5 > 0:
-    print("Notas de 5:{}".format(notas_5))
-if moeda_1 > 0:
-    print("Moedas de 1 real:{}\n".format(moeda_1))
+def printar_atm(notas_100, notas_50, notas_10, notas_5, moeda_1):
+    if notas_100 > 0:
+        print("Notas de 100: {}".format(notas_100))
+    if notas_50 > 0:
+        print("Notas de 50: {}".format(notas_50))
+    if notas_10 > 0: 
+        print("Notas de 10:{}".format(notas_10))
+    if notas_5 > 0:
+        print("Notas de 5:{}".format(notas_5))
+    if moeda_1 > 0:
+        print("Moedas de 1 real:{}\n".format(moeda_1))
