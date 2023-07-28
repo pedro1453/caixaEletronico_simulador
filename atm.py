68b1687420055db5e1239e70821cbd6b4e5731d4
pedro1453 <pedrojc1904@gmail.com>
2023-07-28 10:08:42 -0400

    git add .

--- atm.py
+++ atm.py
@@ -14,4 +14,13 @@ if dinheiro > 10 and dinheiro <= 600:
     moeda_1 = int(dinheiro - (notas_100*100 + notas_50*50 + notas_10*10 + notas_5*5))
 
 print("Para sacar a quantia de R$ {}, será necessário:".format(dinheiro))
-print("Notas de 100: {}\n Notas de 50: {}\nNotas de 10:{}\nNotas de 5:{}\nMoedas de 1 real:{}\n".format(notas_100, notas_50, notas_10,notas_5, moeda_1))
+if notas_100 > 0:
+    print("Notas de 100: {}".format(notas_100))
+if notas_50 > 0:
+    print("Notas de 50: {}".format(notas_50))
+if notas_10 > 0: 
+    print("Notas de 10:{}".format(notas_10))
+if notas_5 > 0:
+    print("Notas de 5:{}".format(notas_5))
+if moeda_1 > 0:
+    print("Moedas de 1 real:{}\n".format(moeda_1))
